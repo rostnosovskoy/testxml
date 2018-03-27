e0c826329f8047766ef269a26f2311933311f257
rostnosovskoy <rostnosovskoy@gmail.com>
2018-03-27 15:12:30 +0300

    Add output data from xml

--- test_xml.php
+++ test_xml.php
@@ -1,1 +1,27 @@
+<!--Выведите на экран столбец пользователей, а рядом с ними - города, в которых они были.-->
+
 <?php
+$xml = file_get_contents('user.xml');
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+echo "<pre>";
+print_r($xml);
+//foreach ($xml as $index => $item) {
+//    echo "<p>$index: $item</p>";
+//}
+
+?>
+
+</body>
+</html>
